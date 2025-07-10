7089c24f63eadd76959d11a21fc5d35bc93b6965
ZYM <2024152884@qq.com>
2025-07-10 16:25:21 +0800

    第一次测试

--- USERCODE/robot_core/tests/test.c
+++ USERCODE/robot_core/tests/test.c
@@ -39,7 +39,8 @@ float set_fric_rpm = FRIC_TEST_RPM; // 摩擦轮设定(rpm)
 float set_dial_rpm = DIAL_RPM;      // 推弹速度设定(rpm)
 float exp_dial_rpm = 0;             // DT7拨轮给的期望
 
-
+float cur_dial_rpm=0;
+float cur_dial_ang=0;
 
 const pid_struct_t test_motor_pid_init_value = {
     .kp = 0.0f,
@@ -60,13 +61,12 @@ float motor_output = 0;
 void motor_test_init(void){
     #if USE_TEST_FILE
     motors_t *motors = get_motors_ptr();
-  memcpy(&dial_rpm_pid, &dial_rpm_pid_init_val, sizeof(pid_struct_t));
-  memcpy(&dial_ang_pid, &dial_ang_pid_init_val, sizeof(pid_struct_t));
+ memcpy(&test_motor_pid, &dial_rpm_pid_init_val, sizeof(pid_struct_t));
 
     //  3   0x203   0x200
-  RM_QUAD_motor_register(&motors->dial, motor_model_RM_QUAD_M2006, QUAD_CURR,
+  RM_QUAD_motor_register(&motors->test, motor_model_RM_QUAD_M2006, QUAD_CURR,
                          COM_CAN, (uint32_t *)&SHOOTER_MOTORS_HCAN, 3, RR_M2006,
-                         &dial_lpf_init_val, M_SHOOTER);
+                         &dial_lpf_init_val, TEST_MOTORS);
 
 
 		can_user_init(
@@ -81,14 +81,35 @@ void motor_test_init(void){
 HAL_StatusTypeDef motor_test_loop(rc_ctrl_t *rc_recv, robot_t *robot){
     #if USE_TEST_FILE
     motors_t *motors = get_motors_ptr(); 
+
+     rc_ctrl_t *dt7_data = get_dt7_data_ptr();///
+
+  float cur_dial_rpm = motors->test.real.rpm / DIAL_RR;///
+  float cur_dial_ang = motors->test.real.abs_angle;
+
     extern float rocker_ry;
     // if(robot->robot_control_flag == DISABLE)
     //     motor_output = 0;
     // else 
     //     motor_output = s_curve(2.0f,rocker_ry);
-    LOAD_MOTOR_TFF(motors->test,motor_output);
+
+    exp_dial_rpm = DIAL_RPM;//
+   set_dial_rpm = exp_dial_rpm;///
+
+
+motor_output = pid_calc(&test_motor_pid, cur_dial_rpm, set_dial_rpm);///
+
+
+    LOAD_MOTOR_TFF(motors->test,motor_output);///
+     motors->test.AUX.EN = true;//////////////////
     return set_all_motor_output(TEST_MOTORS);
     #else
     return HAL_OK;
 		#endif
 }
+//完整流程
+//init  //复制pid memset  获取电机指针    register电机  can init
+
+
+
+//计算pid   计算输出pid_cale    设置电机输出set_all_motor_output(TEST_MOTORS)
